a3fbaa21745276b7ca50e2f3e5a7fb7b510f2c25
Qi Zhang <singularitti@outlook.com>
2018-12-02 03:02:53 -0500

    Add StatMech.jl
    for statistical mechanics functions

---
+++ src/StatMech.jl
@@ -0,0 +1,63 @@
+"""
+# module StatMech
+
+- Julia version: 1.0.2
+- Author: qz
+- Date: 2018-12-02
+
+# Examples
+
+```jldoctest
+julia>
+```
+"""
+module StatMech
+
+const HBAR = 1
+const BOLTZMANN = 2
+
+validate_frequency(frequency) = frequency < 0 && throw(DomainError("Negative frequency is not proper for QHA calculation!"))
+
+bose_einstein_distribution(temperature, frequency) = 1 / (exp(HBAR * frequency / (BOLTZMANN * temperature)) - 1)
+
+function  subsystem_partition_function(temperature, frequency)
+    frequency == 0 && return 1
+
+    x = HBAR * frequency / (BOLTZMANN * temperature)
+    return exp(x / 2) / (exp(x) - 1)
+end
+
+function subsystem_free_energy(temperature, frequency)
+    validate_frequency(frequency)
+    frequency == 0 && return 0
+
+    hw = HBAR * frequency
+    kt = BOLTZMANN * temperature
+    return hw / 2 + kt * log(1 - exp(-hw / kt))
+end
+
+function subsystem_internal_energy(temperature, frequency)
+    validate_frequency(frequency)
+    frequency == 0 && return BOLTZMANN * temperature
+
+    hw = HBAR * frequency
+    return hw / 2 / tanh(hw / (2 * BOLTZMANN * temperature))
+end
+
+function subsystem_entropy(temperature, frequency)
+    validate_frequency(frequency)
+
+    n = bose_einstein_distribution(temperature, frequency)
+    return BOLTZMANN * ((1 + n) * log(1 + n) - n * log(n))
+end
+
+function subsystem_volumetric_specific_heat(temperature, frequency)
+    validate_frequency(frequency)
+    frequency == 0 && return BOLTZMANN
+
+    hw = HBAR * frequency
+    kt = 2 * BOLTZMANN * temperature
+    return BOLTZMANN * (hw / sinh(hw / kt) / kt)^2
+end
+
+end
