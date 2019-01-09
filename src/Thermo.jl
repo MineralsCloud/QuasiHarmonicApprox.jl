ef8b9c009f73d538670424354eed8367ec853672
Qi Zhang <singularitti@outlook.com>
2019-01-08 19:23:38 -0500

    Add Thermo.jl
    bring from QHA.jl

---
+++ src/Thermo.jl
@@ -0,0 +1,55 @@
+"""
+# module Thermo
+
+
+
+# Examples
+
+```jldoctest
+julia>
+```
+"""
+module Thermo
+
+using ArgCheck: @argcheck
+
+import Base: length, ==, size
+
+export NaturalVariable,
+    ThermodynamicField,
+    length, ==, size
+
+const NATURAL_VARIABLE_LABELS = (:T, :S, :P, :V)
+
+const CONJUGATE_PAIRS = (Set([:T, :S]), Set([:P, :V]))
+
+struct NaturalVariable{T}
+    values::Vector
+    function NaturalVariable{T}(values) where {T}
+        @argcheck T in NATURAL_VARIABLE_LABELS
+        new(values)
+    end
+end
+
+length(x::NaturalVariable) = length(x.values)
+
+==(x::T, y::T) where {T <: NaturalVariable} = x.values == y.values
+
+struct ThermodynamicField{A, B} <: BivariateField{A, B}
+    first::NaturalVariable{A}
+    second::NaturalVariable{B}
+    values::Matrix
+    function ThermodynamicField{A, B}(first, second, values) where {A, B}
+        @argcheck A != B
+        @argcheck Set([A, B]) ∉ CONJUGATE_PAIRS
+        @argcheck (length(first), length(second)) == size(values)
+        new(first, second, values)
+    end
+end
+
+ThermodynamicField(first::NaturalVariable{A}, second::NaturalVariable{B}, values::Matrix) where {A, B} = ThermodynamicField{A, B}(first, second, values)
+ThermodynamicField{A, B}(first::Vector, second::Vector, values::Matrix) where {A, B} = ThermodynamicField(NaturalVariable{A}(first), NaturalVariable{B}(second), values)
+
+size(f::ThermodynamicField) = size(f.values)
+
+end
