f666233c32bca33c82deabd4f9b825a7f1ff4a13
Qi Zhang <singularitti@outlook.com>
2019-01-19 22:57:32 +0800

    Separate `sample_brillouin_zone` and its validation
    in Sampling.jl

--- src/Sampling.jl
+++ src/Sampling.jl
@@ -13,17 +13,20 @@ julia>
 """
 module Sampling
 
+using QuasiHarmonicApproximation.AbstractField: whichdimension
 using QuasiHarmonicApproximation.QSpace: QSpaceField
 
 export sample_brillouin_zone
 
-function sample_brillouin_zone(q_weights::Vector{Float64}, quantity::QSpaceField{:q, T}) where {T}
-    length(q_weights) != size(quantity.values, 2) && throw(DimensionMismatch)
+function validate_brillouin_zone_sampling(q_weights::Vector{Float64}, quantity::QSpaceField)
+    length(q_weights) != size(quantity, :q) && throw(DimensionMismatch)
     all(q_weights .>= 0) || throw(DomainError("All the values of the weights should be greater than 0!"))
+end
 
+function sample_brillouin_zone(q_weights::Vector{Float64}, quantity::QSpaceField)
+    validate_brillouin_zone_sampling(q_weights, quantity)
     q_weights /= sum(q_weights)
     return sum(quantity * q_weights)
 end
-sample_brillouin_zone(q_weights::Vector{Float64}, quantity::QSpaceField{T, :q}) where {T} = sample_brillouin_zone(q_weights, quantity)
 
 end
