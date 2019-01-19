d5fd336c9d315e7982c9988ad3257b2bb0bbd76e
Qi Zhang <singularitti@outlook.com>
2019-01-19 22:17:15 +0800

    Simplify `legendre_transformation`

--- src/Thermodynamics.jl
+++ src/Thermodynamics.jl
@@ -17,27 +17,22 @@ using QuasiHarmonicApproximation.Thermo
 
 export legendre_transformation
 
-const BIMAP = Dict(:T => :S, :S => :T, :V => :P, :P => :V)
+function legendre_transformation(f::ThermodynamicField, S::Symbol)
+    conjugate_variable = -differentiate(f, S)
 
-function legendre_transformation(f::ThermodynamicField{A, B}, T::Symbol) where {A, B}
-    @argcheck T in (A, B)
-    conjugate = -differentiate(f, T)
+    function (interpolator::Function, desired_variable::NaturalVariable{T}, scheme::Int = 1) where {T}
+        @argcheck Set([S, T]) in QuasiHarmonicApproximation.Thermo.CONJUGATE_PAIRS
 
-    function (interpolator::Function, desired_conjugate::Vector{Float64})
-        x = interpolator(conjugate, f.values + conjugate * f.second)
-        y = x(desired_conjugate)
-        ThermodynamicField(f.first, NaturalVariable{BIMAP[T]}(desired_conjugate.values), y)
-    end
-end
-
-function legendre_transformation(f::ThermodynamicField{A, B}, T::Symbol) where {A, B}
-    @argcheck T in (A, B)
-    conjugate = -differentiate(f, T)
-
-    function (interpolator::Function, desired_conjugate::Vector{Float64})
-        x = interpolator(conjugate, f.values)
-        y = x(desired_conjugate)
-        ThermodynamicField(f.first, NaturalVariable{BIMAP[T]}(desired_conjugate.values), y + desired_conjugate * f.second)
+        if scheme == 1
+            x = interpolator(conjugate_variable, f.values + conjugate_variable * f.second)
+            y = x(desired_variable)
+        elseif scheme == 2
+            x = interpolator(conjugate_variable, f.values)
+            y = x(desired_variable) + desired_variable * f.second
+        else
+            throw(ArgumentError("The interpolation `scheme` is either `1` or `2`, with `$scheme` provided!"))
+        end
+        ThermodynamicField(f.first, desired_variable, y)
     end
 end
 
