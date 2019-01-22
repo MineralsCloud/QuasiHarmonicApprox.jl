1c7f61fe950ad228eb60c3c071282c38c9baf02d
Qi Zhang <singularitti@outlook.com>
2019-01-22 07:14:12 -0500

    Add src/Interpolation.jl

--- src/Interpolation.jl
+++ src/Interpolation.jl
@@ -13,4 +13,22 @@ module Interpolation
 
 using Interpolations
 
+using QuasiHarmonicApproximation.AbstractField: AbstractVariable, BivariateField, whichdimension, getvariable
+
+export Interpolator,
+    interpolate
+
+struct Interpolator
+    interpolating_function::Function
+    logger::Union{Logger, Nothing}
+end
+
+function interpolate(x::BivariateField, y::BivariateField, interpolator::Interpolator)::Function
+    func = interpolator(x.values, y.values)
+
+    function (to_variable::NaturalVariable)
+        func(to_variable)
+    end
+end
+
 end
