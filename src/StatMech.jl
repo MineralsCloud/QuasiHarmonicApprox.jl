c26045be7f1b19ca60e4e5610c7ef66e284d237c
Qi Zhang <singularitti@outlook.com>
2019-01-22 12:10:47 -0500

    Simplify type signatures of StatMech.jl

--- src/StatMech.jl
+++ src/StatMech.jl
@@ -23,18 +23,18 @@ export bose_einstein_distribution,
 const HBAR = 1.0
 const BOLTZMANN = 2.0
 
-validate_frequency(frequency::Float64) = frequency < 0 && throw(DomainError("Negative frequency is not proper for QHA calculation!"))
+validate_frequency(frequency::AbstractFloat) = frequency < 0 && throw(DomainError("Negative frequency is not proper for QHA calculation!"))
 
-bose_einstein_distribution(temperature::Float64, frequency::Float64)::Float64 = 1 / (exp(HBAR * frequency / (BOLTZMANN * temperature)) - 1)
+bose_einstein_distribution(temperature::T, frequency::T)::T where {T <: AbstractFloat} = 1 / (exp(HBAR * frequency / (BOLTZMANN * temperature)) - 1)
 
-function subsystem_partition_function(temperature::Float64, frequency::Float64)::Float64
+function subsystem_partition_function(temperature::T, frequency::T)::T where {T <: AbstractFloat}
     frequency == 0 && return 1
 
     x = HBAR * frequency / (BOLTZMANN * temperature)
     return exp(x / 2) / (exp(x) - 1)
 end
 
-function subsystem_free_energy(temperature::Float64, frequency::Float64)::Float64
+function subsystem_free_energy(temperature::T, frequency::T)::T where {T <: AbstractFloat}
     validate_frequency(frequency)
     frequency == 0 && return 0
 
@@ -43,7 +43,7 @@ function subsystem_free_energy(temperature::Float64, frequency::Float64)::Float6
     return hw / 2 + kt * log(1 - exp(-hw / kt))
 end
 
-function subsystem_internal_energy(temperature::Float64, frequency::Float64)::Float64
+function subsystem_internal_energy(temperature::T, frequency::T)::T where {T <: AbstractFloat}
     validate_frequency(frequency)
     frequency == 0 && return BOLTZMANN * temperature
 
@@ -51,14 +51,14 @@ function subsystem_internal_energy(temperature::Float64, frequency::Float64)::Fl
     return hw / 2 / tanh(hw / (2 * BOLTZMANN * temperature))
 end
 
-function subsystem_entropy(temperature::Float64, frequency::Float64)::Float64
+function subsystem_entropy(temperature::T, frequency::T)::T where {T <: AbstractFloat}
     validate_frequency(frequency)
 
     n = bose_einstein_distribution(temperature, frequency)
     return BOLTZMANN * ((1 + n) * log(1 + n) - n * log(n))
 end
 
-function subsystem_volumetric_specific_heat(temperature::Float64, frequency::Float64)::Float64 
+function subsystem_volumetric_specific_heat(temperature::T, frequency::T)::T where {T <: AbstractFloat}
     validate_frequency(frequency)
     frequency == 0 && return BOLTZMANN
 
