329af9d33b10d0aaa2e64b76620413f2dad9189c
Qi Zhang <singularitti@outlook.com>
2019-01-22 07:14:57 -0500

    Modify `legendre_transformation` slightly

--- src/Thermodynamics.jl
+++ src/Thermodynamics.jl
@@ -14,25 +14,26 @@ module Thermodynamics
 using ArgCheck: @argcheck
 
 using QuasiHarmonicApproximation.Thermo
+using QuasiHarmonicApproximation.Interpolation: Interpolator, interpolate
 
 export legendre_transformation
 
-function legendre_transformation(f::ThermodynamicField, S::Symbol)
-    conjugate_variable = -differentiate(f, S)
+function legendre_transformation(f::ThermodynamicField, s::Symbol)
+    conjugate_variable = -differentiate(f, s)
 
-    function (interpolator::Function, desired_variable::NaturalVariable{T}, scheme::Int = 1) where {T}
+    function (interpolator::Interpolator, to_variable::NaturalVariable{T}, scheme::Int = 1) where {T}
         @argcheck Set([S, T]) in QuasiHarmonicApproximation.Thermo.CONJUGATE_PAIRS
 
         if scheme == 1
-            x = interpolator(conjugate_variable, f.values + conjugate_variable * f.second)
-            y = x(desired_variable)
+            x = interpolate(conjugate_variable, f + conjugate_variable * f.second, interpolator)
+            y = x(to_variable)
         elseif scheme == 2
-            x = interpolator(conjugate_variable, f.values)
-            y = x(desired_variable) + desired_variable * f.second
+            x = interpolate(conjugate_variable, f, interpolator)
+            y = x(to_variable) + to_variable * f.second
         else
             throw(ArgumentError("The interpolation `scheme` is either `1` or `2`, with `$scheme` provided!"))
         end
-        ThermodynamicField(f.first, desired_variable, y)
+        ThermodynamicField(f.first, to_variable, y)
     end
 end
 
