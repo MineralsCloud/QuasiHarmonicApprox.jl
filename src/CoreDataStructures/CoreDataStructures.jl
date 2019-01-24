02ad9b0f1579461d1df2d5513cfa16a298b8c2f7
Qi Zhang <singularitti@outlook.com>
2019-01-24 15:06:07 -0500

    Using Reexport

--- src/CoreDataStructures/CoreDataStructures.jl
+++ src/CoreDataStructures/CoreDataStructures.jl
@@ -11,8 +11,15 @@ julia>
 """
 module CoreDataStructures
 
+using Reexport
+
 include("AbstractField.jl")
+@reexport using .AbstractField
+
 include("QSpace.jl")
+@reexport using .QSpace
+
 include("Thermo.jl")
+@reexport using .Thermo
 
 end
