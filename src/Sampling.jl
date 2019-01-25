1d9dcdd0b22d3f301bdf0520b7cb3a176f1a75a8
Qi Zhang <singularitti@outlook.com>
2019-01-25 07:32:35 -0500

    Fix Sampling.jl

--- src/Sampling.jl
+++ src/Sampling.jl
@@ -13,20 +13,20 @@ julia>
 """
 module Sampling
 
-using QuasiHarmonicApproximation.CoreDataStructures.Abstractions: whichdimension
-using QuasiHarmonicApproximation.CoreDataStructures.QSpace: QSpaceField
+using QuasiHarmonicApproximation.CoreDataStructures.Abstractions: whichaxis
+using QuasiHarmonicApproximation.CoreDataStructures.QSpace: NormalMode, QSpaceField
 
 export sample_brillouin_zone
 
-function validate_brillouin_zone_sampling(q_weights::AbstractVector{<: AbstractFloat}, quantity::QSpaceField)
+function validate_brillouin_zone_sampling(q_weights::NormalMode{:q}, quantity::QSpaceField)
     length(q_weights) != size(quantity, :q) && throw(DimensionMismatch)
     all(q_weights .>= 0) || throw(DomainError("All the values of the weights should be greater than 0!"))
 end
 
-function sample_brillouin_zone(q_weights::AbstractVector{<: AbstractFloat}, quantity::QSpaceField)
+function sample_brillouin_zone(q_weights::T, quantity::QSpaceField) where {T <: NormalMode{:q}}
     validate_brillouin_zone_sampling(q_weights, quantity)
-    q_weights /= sum(q_weights)
-    return sum(quantity * q_weights)
+    var = T(q_weights.values / sum(q_weights.values))
+    return sum(quantity * var)
 end
 
 end
