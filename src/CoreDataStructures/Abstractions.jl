78733c918f21ee04ffb266785c31c2694a88d3fb
Qi Zhang <singularitti@outlook.com>
2019-02-14 23:54:06 -0500

    Add more functions to Abstractions.jl

--- src/CoreDataStructures/Abstractions.jl
+++ src/CoreDataStructures/Abstractions.jl
@@ -14,14 +14,16 @@ module Abstractions
 using MacroTools: @forward
 using Setfield: @set
 
-import Base: length, size,
-    ==, *
+import Base: axes, length, size, ==, *
 
 export Axis,
     Axes,
     Field,
-    length, size,
-    ==, *
+    axes,
+    axisnames,
+    axisdim,
+    axisvalues,
+    length, size, ==, *
 
 abstract type Axis{a, A} end
 
@@ -29,16 +31,33 @@ const Axes{a, b, A, B} = Tuple{Axis{a, A}, Axis{b, B}}
 
 abstract type Field{a, b, A, B, T} end
 
+axes(field::Field) = field.axes
+axes(field::Field, dim::Int) = axes(field)[dim]
+
+axisnames(axis::Type{<: Axis{a}}) where {a} = a
+axisnames(axis::Axis) = axisnames(typeof(axis))
+axisnames(axes::Type{<: Axes{a, b}}) where {a, b} = (a, b)
+axisnames(axes::Axes) = axisnames(typeof(axes))
+axisnames(field::Type{<: Field{a, b}}) where {a, b} = (a, b)
+axisnames(field::Field) = axisnames(typeof(field))
+
+function axisdim(F::Type{<: Field}, A::Type{<: Axis})::Int
+    index = findfirst(isequal(axisnames(A)), axisnames(F))
+    isa(index, Nothing) ? error() : index
+end
+function axisdim(field::Field, axis::Axis)::Int
+    index = axisdim(typeof(field), typeof(axis))
+    field.axes[index] == axis ? index : error()
+end
+
+axisvalues(field::Field) = axisvalues(field.axes...)
+axisvalues(axis::Axis, axes::Axis...) = tuple(axis.data, axisvalues(axes...)...)
+
 @forward Axis.data length, size, ==
 
 function *(field::T, axis::Axis)::T where {T <: Field}
-    axisname = whichaxis_iscompatible(field, axis)
-
-    @set field.data = if axisname == :first
-        field.data .* axis.data
-    else
-        field.data .* transpose(axis.data)
-    end
+    dim = axisdim(field, axis)
+    @set field.data = (dim == 1 ? field.data .* axis.data : field.data .* transpose(axis.data))
 end
 *(v::Axis, field::Field) = *(field, v)  # Make it valid on both direction
 
