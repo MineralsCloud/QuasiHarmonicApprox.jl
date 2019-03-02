8e17a4850ec9255575c3bc552ee41178157072db
Qi Zhang <singularitti@outlook.com>
2019-03-02 00:05:34 -0500

    style: Reformat documents

--- src/CoreDataStructures/QSpace.jl
+++ src/CoreDataStructures/QSpace.jl
@@ -18,24 +18,24 @@ export NormalMode,
 
 const NORMAL_MODE_LABELS = (:q, :s)
 
-struct NormalMode{a, A <: AbstractVector} <: Axis{a, A}
+struct NormalMode{a,A <: AbstractVector} <: Axis{a,A}
     data::A
-    function NormalMode{a, A}(data) where {a, A}
+    function NormalMode{a,A}(data) where {a,A}
         @assert a ∈ NORMAL_MODE_LABELS
         new(data)
     end
 end
-NormalMode{a}(data::A) where {a, A} = NormalMode{a, A}(data)
+NormalMode{a}(data::A) where {a,A} = NormalMode{a,A}(data)
 
-struct QSpaceField{a, b, A, B, T <: AbstractMatrix} <: Field{a, b, A, B, T}
-    axes::Axes{a, b, A, B}
+struct QSpaceField{a,b,A,B,T <: AbstractMatrix} <: Field{a,b,A,B,T}
+    axes::Axes{a,b,A,B}
     data::T
-    function QSpaceField{a, b, R, S, T}(axes, data) where {a, b, R, S, T}
+    function QSpaceField{a,b,R,S,T}(axes, data) where {a,b,R,S,T}
         @assert map(length, axes) == size(data)
         new(axes, data)
     end
 end
-QSpaceField(axes::Axes{a, b, A, B}, data::T) where {a, b, A, B, T} = QSpaceField{a, b, A, B, T}(axes, data)
+QSpaceField(axes::Axes{a,b,A,B}, data::T) where {a,b,A,B,T} = QSpaceField{a,b,A,B,T}(axes, data)
 QSpaceField(first::NormalMode, second::NormalMode, data) = QSpaceField((first, second), data)
 
 end
--- src/CoreDataStructures/Thermo.jl
+++ src/CoreDataStructures/Thermo.jl
@@ -24,25 +24,25 @@ export NaturalVariable,
 const NATURAL_VARIABLE_LABELS = (:T, :S, :P, :V)
 const CONJUGATE_PAIRS = Dict(:T => :S, :P => :V, :S => :T, :V => :P)
 
-struct NaturalVariable{a, A <: AbstractVector} <: Axis{a, A}
+struct NaturalVariable{a,A <: AbstractVector} <: Axis{a,A}
     data::A
-    function NaturalVariable{a, A}(data) where {a, A}
+    function NaturalVariable{a,A}(data) where {a,A}
         @assert a ∈ NATURAL_VARIABLE_LABELS
         new(data)
     end
 end
-NaturalVariable{a}(data::A) where {a, A} = NaturalVariable{a, A}(data)
+NaturalVariable{a}(data::A) where {a,A} = NaturalVariable{a,A}(data)
 
-struct ThermodynamicField{a, b, A, B, T <: AbstractMatrix} <: Field{a, b, A, B, T}
-    axes::Axes{a, b, A, B}
+struct ThermodynamicField{a,b,A,B,T <: AbstractMatrix} <: Field{a,b,A,B,T}
+    axes::Axes{a,b,A,B}
     data::T
-    function ThermodynamicField{a, b, A, B, T}(axes, data) where {a, b, A, B, T}
+    function ThermodynamicField{a,b,A,B,T}(axes, data) where {a,b,A,B,T}
         @assert map(length, axes) == size(data)
         @assert (a => b) ∉ CONJUGATE_PAIRS
         new(axes, data)
     end
 end
-ThermodynamicField(axes::Axes{a, b, A, B}, data::T) where {a, b, A, B, T} = ThermodynamicField{a, b, A, B, T}(axes, data)
+ThermodynamicField(axes::Axes{a,b,A,B}, data::T) where {a,b,A,B,T} = ThermodynamicField{a,b,A,B,T}(axes, data)
 ThermodynamicField(first::NaturalVariable, second::NaturalVariable, data) = ThermodynamicField((first, second), data)
 
 get_conjugate_variable_name(name::Symbol)::Symbol = CONJUGATE_PAIRS[name]
