7d99eecfa52ee820ead501e9c5445eb96c0f9109
Qi Zhang <singularitti@outlook.com>
2019-03-02 00:46:09 -0500

    Deprecate `Setfield` in Sampling.jl
    Since we have type-stable `map` now.

--- src/Sampling.jl
+++ src/Sampling.jl
@@ -13,8 +13,6 @@ julia>
 """
 module Sampling
 
-using Setfield: @set
-
 using QuasiHarmonicApproximation.CoreDataStructures
 
 export sample_brillouin_zone
@@ -22,13 +20,13 @@ export sample_brillouin_zone
 function validate_brillouin_zone_sampling(q_weights::NormalMode{:q}, quantity::QSpaceField)
     m, n = length(q_weights), length(axes(quantity, 1))
     m != n && throw(DimensionMismatch("The number of q-points $m does not match $n!"))
-    all(q_weights.data .>= 0) || throw(DomainError("All the values of the weights should be greater than 0!"))
+    all(axisvalues(q_weights) .>= 0) || throw(DomainError("All the values of the weights should be greater than 0!"))
 end
 
 function sample_brillouin_zone(q_weights::T, quantity::QSpaceField) where {T <: NormalMode{:q}}
     validate_brillouin_zone_sampling(q_weights, quantity)
-    quantity = (@set q_weights.data /= sum(q_weights.data))
-    return sum(*(quantity, q_weights))
+    factor = sum(axisvalues(q_weights))
+    return sum(quantity * map(x -> x / factor, q_weights))
 end
 
 end
