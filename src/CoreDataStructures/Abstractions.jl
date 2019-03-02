1309ee7a66833c5a2385431df1d211d2924d3265
Qi Zhang <singularitti@outlook.com>
2019-03-01 21:05:38 -0500

    Deprecate the use of `@forward`
    Write forwarded methods by hand

--- src/CoreDataStructures/Abstractions.jl
+++ src/CoreDataStructures/Abstractions.jl
@@ -11,7 +11,6 @@ julia>
 """
 module Abstractions
 
-using MacroTools: @forward
 using Setfield: @set
 
 export Axis,
@@ -33,6 +32,9 @@ Base.axes(field::Field) = field.axes
 Base.axes(field::Field, dim::Int) = axes(field)[dim]
 Base.axes(field::Field, A::Type{<: Axis}) = axes(field, axisdim(A))
 Base.axes(field::Field, axis::Axis) = axes(field, axisdim(field, axis))
+
+axistype(::Type{<: Axis{a, A}}) where {a, A} = A
+axistype(axis::Axis) = axistype(typeof(axis))
 
 axisnames(::Type{<: Axis{a}}) where {a} = a
 axisnames(axis::Axis) = axisnames(typeof(axis))
@@ -60,12 +62,30 @@ end
 
 Base.transpose(field::Field) = typeof(field).name.wrapper(reverse(axes(field)), transpose(field.data))
 
-@forward Axis.data Base.length, Base.size, Base.:(==)
-
 function Base.:*(field::T, axis::Axis)::T where {T <: Field}
     dim = axisdim(field, axis)
     @set field.data = (dim == 1 ? field.data .* axis.data : field.data .* transpose(axis.data))
 end
 Base.:*(v::Axis, field::Field) = *(field, v)  # Make it valid on both direction
+
+Base.:(==)(A::Axis{a}, B::Axis{a}) where {a} = A.data == B.data
+
+Base.eltype(::Type{<: Axis{a, A}}) where {a, A} = eltype(A)
+Base.eltype(axis::Axis) = eltype(typeof(axis))
+
+Base.getindex(axis::Axis, i...) = getindex(axis.data, i...)
+
+Base.firstindex(axis::Axis) = firstindex(axis.data)
+
+Base.lastindex(axis::Axis) = length(axis)
+
+Base.size(axis::Axis) = size(axis.data)
+
+Base.length(axis::Axis) = length(axis.data)
+
+Base.iterate(axis::Axis) = (axis, nothing)
+Base.iterate(::Axis, ::Any) = nothing
+Base.iterate(::Type{T}) where {T <: Axis} = (T, nothing)
+Base.iterate(::Type{<: Axis}, ::Any) = nothing
 
 end
