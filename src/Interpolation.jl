cc1a1eab19396e08aee109c94358b1da55446496
Qi Zhang <singularitti@outlook.com>
2019-03-03 11:07:56 -0500

    Redesign Interpolation.jl

--- src/Interpolation.jl
+++ src/Interpolation.jl
@@ -15,40 +15,30 @@ using Interpolations
 
 using QuasiHarmonicApproximation.CoreDataStructures
 
-export Interpolator,
-    interpolate
+export AbstractInterpolator,
+    NDInterpolator
 
 abstract type AbstractInterpolator end
 
-struct Interpolator{T} <: AbstractInterpolator
-    interpolating_function
+struct NDInterpolator{N} <: AbstractInterpolator
+    f::Function
 end
 
-function (interpolator::Interpolator{1})(x, y, s::Symbol)
-    iscompatible(x, y) && error()
-    axis = whichaxis(x, s)
-    dim = (axis == :first ? 1 : 2)
-    m, n = size(x.values)
+function (interpolator::NDInterpolator{1})(x::Field, y::Field, axis::Type{<: Axis})
+    m, n = map(length, x.axes)
+    dim = axisdim(typeof(y), axis)
 
-    interps = []
+    result = []
     if dim == 1
         for i in 1:n
-            push!(interps, interpolator.interpolating_function(x[:, i], y[:, i]))
+            push!(result, interpolator.f(x[:, i], y[:, i]))
         end
     elseif dim == 2
         for j in 1:m
-            push!(interps, interpolator.interpolating_function(x[j, :], y[j, :]))
+            push!(result, interpolator.f(x[j, :], y[j, :]))
         end
     end
-    interps
-end
-
-function interpolate(x::BiaxialField, y::BiaxialField, interpolator::Interpolator{1}, s::Symbol)::Function
-    interps = interpolator(x, y, s)
-
-    function (to_variable::AbstractAxis, dim::Int)
-        map(interps, to_variable.values)
-    end
+    result
 end
 
 end
