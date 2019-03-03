59b1a243869793a58084a2e1785601d2ca5b8f54
Qi Zhang <singularitti@outlook.com>
2019-03-03 11:07:43 -0500

    Redesign Thermodynamics.jl

--- src/Thermodynamics.jl
+++ src/Thermodynamics.jl
@@ -14,16 +14,16 @@ module Thermodynamics
 using Setfield: @set
 
 using QuasiHarmonicApproximation.CoreDataStructures
-using QuasiHarmonicApproximation.Interpolation: Interpolator, interpolate
+using QuasiHarmonicApproximation.Interpolation
 
 export legendre_transformation
 
-function legendre_transformation(field::ThermodynamicField, new_variable::NaturalVariable)
-    name = get_conjugate_variable_name(axisnames(new_variable))
-    conjugate_variable = get_conjugate_variable(field, name)
+function legendre_transformation(field::ThermodynamicField, new_variable::NaturalVariable) # F(T, V), P
+    name = get_conjugate_variable_name(axisnames(new_variable))  # V
+    conjugate_variable = get_conjugate_variable(field, name)  # V axis
 
-    function (interpolator::Interpolator)
-        f = interpolate(conjugate_variable, conjugate_variable * new_variable - field, interpolator)
+    function (interpolator::AbstractInterpolator)
+        f = interpolator.f(conjugate_variable, conjugate_variable * new_variable - field, NaturalVariable{name})  # V. V P - F(T, V), V
         ThermodynamicField(replaceaxis(axes(field), new_variable), f(new_variable))
     end
 end
