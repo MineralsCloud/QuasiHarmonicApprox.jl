56130d62e4caf77e3b41ee4c97704a0359040d4e
Qi Zhang <singularitti@outlook.com>
2019-03-10 01:49:16 -0500

    Fix wrong logic in `legendre_transformation`

--- src/Thermodynamics.jl
+++ src/Thermodynamics.jl
@@ -19,11 +19,12 @@ using QuasiHarmonicApproximation.Interpolation
 export legendre_transformation
 
 function legendre_transformation(field::ThermodynamicField, new_variable::NaturalVariable) # F(T, V), P
-    name = get_conjugate_variable_name(axisnames(new_variable))  # V
-    conjugate_variable = get_conjugate_variable(field, name)  # V axis
+    name = get_conjugate_variable_name(axisnames(new_variable))  # :V
+    conjugate_variable = get_conjugate_variable(field, NaturalVariable{name})  # P(T, V)
 
     function (interpolator::AbstractInterpolator)
-        f = interpolator.f(conjugate_variable, conjugate_variable * new_variable - field, NaturalVariable{name})  # V. V P - F(T, V), V
+        println(axes(field, NaturalVariable{name}))
+        f = interpolator(conjugate_variable, conjugate_variable * axes(field, NaturalVariable{name}) - field, NaturalVariable{name})  # P. P(T, V) * V - F(T, V), V
         ThermodynamicField(replaceaxis(axes(field), new_variable), f(new_variable))
     end
 end
