a107b36a426e25d1dda08b30c7292fdf960b85e9
Qi Zhang <singularitti@outlook.com>
2019-03-30 22:19:48 -0400

    Fix and reformat `deepflatten`

--- src/Tools.jl
+++ src/Tools.jl
@@ -43,22 +43,20 @@ function differentiate(x::T, f::T, dim::Int)::T where {T <: AbstractMatrix}
     derivative
 end
 
-function deepflatten(arr)
+function deepflatten(arr::Vector{<: Vector})
     dim = [1]
 
     function recursiveflatten(arr, dim)
-        if isa(arr, Vector{<: Vector})
-            recursiveflatten(
-                collect(Iterators.flatten(arr)),
-                pushfirst!(dim, length(arr) / prod(dim))
-            )
+        if arr isa Vector{<: Vector}
+            recursiveflatten(collect(Iterators.flatten(arr)),
+                pushfirst!(dim, length(arr) / prod(dim)))
         else
             arr, pushfirst!(dim, length(arr) / prod(dim))
         end
     end
 
     flattened, dim = recursiveflatten(arr, dim)
-    reshape(flattened, dim[1:end-1]...)
+    reshape(flattened, dim[1:end - 1]...)
 end
 
 end
