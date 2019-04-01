01975af2e7b1dd080e17da8d718427d5b5c79661
Qi Zhang <singularitti@outlook.com>
2019-03-31 21:15:17 -0400

    Rewrite Sampling.jl
    Clarify: `q_weights` should be a `QSpaceField`, not a vector!

--- src/Sampling.jl
+++ src/Sampling.jl
@@ -17,16 +17,15 @@ using QuasiHarmonicApproximation.CoreDataStructures
 
 export sample_brillouin_zone
 
-function validate_brillouin_zone_sampling(q_weights::NormalMode{:q}, quantity::QSpaceField)
-    m, n = length(q_weights), length(axes(quantity, 1))
-    m != n && throw(DimensionMismatch("The number of q-points $m does not match $n!"))
-    all(axisvalues(q_weights) .>= 0) || throw(DomainError("All the values of the weights should be greater than 0!"))
+function validate_brillouin_zone_sampling(q_weights::T, quantity::T) where {T <: QSpaceField}
+    size(q_weights) != size(quantity) && throw(DimensionMismatch("The number of q-points does not match!"))
+    all(q_weights .>= 0) || throw(DomainError("All the values of the weights should be greater than 0!"))
 end
 
-function sample_brillouin_zone(q_weights::T, quantity::QSpaceField) where {T <: NormalMode{:q}}
+function sample_brillouin_zone(q_weights::T, quantity::T) where {T <: QSpaceField}
     validate_brillouin_zone_sampling(q_weights, quantity)
-    factor = sum(axisvalues(q_weights))
-    return sum(quantity * map(x -> x / factor, q_weights))
+    factor = sum(q_weights)
+    sum(quantity * q_weights) / factor  # * is elementwise
 end
 
 end
