7f66128bbfb32c6086812c8d9393aeda216b27ff
Qi Zhang <singularitti@outlook.com>
2019-10-20 06:57:11 -0400

    Make methods in src/StatisticalMechanics.jl lazy
    what I learned in EquationsOfState.jl

--- src/StatisticalMechanics.jl
+++ src/StatisticalMechanics.jl
@@ -12,16 +12,19 @@ export bose_einstein_distribution,
 validate_frequency(ω) =
     ω < zero(ω) && throw(DomainError("Negative frequency is not proper for QHA calculation!"))
 
+bose_einstein_distribution(t) = ω -> bose_einstein_distribution(t, ω)
 function bose_einstein_distribution(t, ω)
     1 / (exp(ħ * ω / (k * t)) - 1)
 end
 
+subsystem_partition_function(t) = ω -> subsystem_partition_function(t, ω)
 function subsystem_partition_function(t, ω)
     ω == 0 && return 1
     x = ħ * ω / (k * t)
     return exp(x / 2) / (exp(x) - 1)
 end
 
+subsystem_free_energy(t) = ω -> subsystem_free_energy(t, ω)
 function subsystem_free_energy(t, ω)
     validate_frequency(ω)
     ω == 0 && return 0
@@ -29,6 +32,7 @@ function subsystem_free_energy(t, ω)
     return ħω / 2 + kt * log(1 - exp(-ħω / kt))
 end
 
+subsystem_internal_energy(t) = ω -> subsystem_internal_energy(t, ω)
 function subsystem_internal_energy(t, ω)
     validate_frequency(ω)
     ω == 0 && return k * t
@@ -36,12 +40,14 @@ function subsystem_internal_energy(t, ω)
     return ħω / 2 * coth(ħω / (2k * t))
 end
 
+subsystem_entropy(t) = ω -> subsystem_entropy(t, ω)
 function subsystem_entropy(t, ω)
     validate_frequency(ω)
     n = bose_einstein_distribution(t, ω)
     return k * ((1 + n) * log(1 + n) - n * log(n))
 end
 
+subsystem_volumetric_specific_heat(t) = ω -> subsystem_volumetric_specific_heat(t, ω)
 function subsystem_volumetric_specific_heat(t, ω)
     validate_frequency(ω)
     ω == 0 && return k
