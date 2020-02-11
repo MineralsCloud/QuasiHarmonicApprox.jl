416e73f41035c29c2070ed52912c1ef85e9c7a6c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-11 01:40:36 +0000

    build based on f0b04a1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#","page":"Home","title":"Home","text":"CurrentModule = QuasiHarmonicApproximation","category":"page"},{"location":"index.html#QuasiHarmonicApproximation-1","page":"Home","title":"QuasiHarmonicApproximation","text":"","category":"section"},{"location":"index.html#","page":"Home","title":"Home","text":"","category":"page"},{"location":"index.html#","page":"Home","title":"Home","text":"Modules = [QuasiHarmonicApproximation]","category":"page"}]
+}
