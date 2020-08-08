7426902da17e980f4989ef54147b96ec19325e10
singularitti <singularitti@outlook.com>
2020-08-08 04:15:09 -0400

    Make `t::Temperature` & `ω::Frequency`

--- src/StatMech.jl
+++ src/StatMech.jl
@@ -1,29 +1,29 @@
 module StatMech
 
 using OptionalArgChecks: @argcheck
-using Unitful: ħ, k, NoUnits
+using Unitful: NoUnits, Temperature, Frequency, ħ, k
 
 export bose_einstein_distribution,
     partition_function, free_energy, internal_energy, entropy, volumetric_specific_heat
 
-function bose_einstein_distribution(t, ω)
+function bose_einstein_distribution(t::Temperature, ω::Frequency)
     @argcheck ω >= zero(ω)
     return 1 / expm1(ħ * ω / (k * t))
 end
 
-function partition_function(t, ω)
+function partition_function(t::Temperature, ω::Frequency)
     @argcheck ω >= zero(ω)
     x = ħ * ω / (k * t)
     return exp(x / 2) / expm1(x)
 end
 
-function free_energy(t, ω)
+function free_energy(t::Temperature, ω::Frequency)
     @argcheck ω >= zero(ω)
     ħω, kt = ħ * ω, k * t
     return -ħω / 2 + kt * log(expm1(ħω / kt))
 end
 
-function internal_energy(t, ω)
+function internal_energy(t::Temperature, ω::Frequency)
     @argcheck ω >= zero(ω)
     if iszero(ω)
         return k * t
@@ -33,12 +33,12 @@ function internal_energy(t, ω)
     end
 end
 
-function entropy(t, ω)
+function entropy(t::Temperature, ω::Frequency)
     n = bose_einstein_distribution(t, ω)
     return k * ((1 + n) * log1p(n) - n * log(n))
 end
 
-function volumetric_specific_heat(t, ω)
+function volumetric_specific_heat(t::Temperature, ω::Frequency)
     @argcheck ω >= zero(ω)
     if iszero(ω)
         return k
