0d2994b6939543566342307bd15da8d4aed18082
singularitti <singularitti@outlook.com>
2020-08-07 20:17:45 -0400

    Replace `validate_frequency` with `OptionalArgChecks.@argcheck`

--- src/StatisticalMechanics.jl
+++ src/StatisticalMechanics.jl
@@ -1,16 +1,14 @@
 module StatisticalMechanics
 
+using OptionalArgChecks: @argcheck
 using Unitful: ħ, k
 
 export bose_einstein_distribution,
-       subsystem_partition_function,
-       subsystem_free_energy,
-       subsystem_internal_energy,
-       subsystem_entropy,
-       subsystem_volumetric_specific_heat
-
-validate_frequency(ω) =
-    ω < zero(ω) && throw(DomainError("Negative frequency is not proper for QHA calculation!"))
+    subsystem_partition_function,
+    subsystem_free_energy,
+    subsystem_internal_energy,
+    subsystem_entropy,
+    subsystem_volumetric_specific_heat
 
 bose_einstein_distribution(t) = ω -> bose_einstein_distribution(t, ω)
 function bose_einstein_distribution(t, ω)
@@ -26,7 +24,7 @@ end
 
 subsystem_free_energy(t) = ω -> subsystem_free_energy(t, ω)
 function subsystem_free_energy(t, ω)
-    validate_frequency(ω)
+    @argcheck ω < zero(ω)
     ω == 0 && return 0
     ħω, kt = ħ * ω, k * t
     return ħω / 2 + kt * log(1 - exp(-ħω / kt))
@@ -34,7 +32,7 @@ end
 
 subsystem_internal_energy(t) = ω -> subsystem_internal_energy(t, ω)
 function subsystem_internal_energy(t, ω)
-    validate_frequency(ω)
+    @argcheck ω < zero(ω)
     ω == 0 && return k * t
     ħω = ħ * ω
     return ħω / 2 * coth(ħω / (2k * t))
@@ -42,14 +40,14 @@ end
 
 subsystem_entropy(t) = ω -> subsystem_entropy(t, ω)
 function subsystem_entropy(t, ω)
-    validate_frequency(ω)
+    @argcheck ω < zero(ω)
     n = bose_einstein_distribution(t, ω)
     return k * ((1 + n) * log(1 + n) - n * log(n))
 end
 
 subsystem_volumetric_specific_heat(t) = ω -> subsystem_volumetric_specific_heat(t, ω)
 function subsystem_volumetric_specific_heat(t, ω)
-    validate_frequency(ω)
+    @argcheck ω < zero(ω)
     ω == 0 && return k
     x = ħ * ω / (2k * t)
     return k * (x * csch(x))^2
