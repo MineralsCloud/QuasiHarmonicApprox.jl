8f41614573c3b90c489e09b76dd81101f7c0da0a
singularitti <singularitti@outlook.com>
2020-08-09 00:19:41 -0400

    Implement `free_energy` for `Frequencies`

--- src/SingleConfiguration.jl
+++ src/SingleConfiguration.jl
@@ -1,5 +1,6 @@
 module SingleConfiguration
 
+using DimensionalData
 using EquationsOfState.Collections
 using EquationsOfState.NonlinearFitting
 using EquationsOfState.Find
@@ -8,30 +9,28 @@ using Unitful: Temperature, Frequency, Energy, @u_str
 
 import ..StatMech: free_energy
 
-function free_energy(
-    t::Temperature,
-    ω::AbstractArray{<:Frequency,3},
-    wₖ::AbstractVector,
-    e0::AbstractVector{<:Energy} = zeros(size(ω, 3)) * u"eV",
-)
+export WaveVector, Branch, Frequencies, Weights, times
+
+@dim WaveVector "WaveVector"
+@dim Branch "Branch"
+
+Frequencies = DimensionalArray{
+    T,
+    2,
+    <:Union{Tuple{WaveVector,Branch},Tuple{Branch,WaveVector}},
+} where {T}
+
+Weights = DimensionalArray{T,1,<:Tuple{WaveVector}} where {T}
+
+function free_energy(t::Temperature, ω::Frequencies, wₖ::Weights, e0::Energy = 0u"eV")
     @argcheck all(w >= zero(w) for w in wₖ)
     wₖ = wₖ / sum(wₖ)
     f = map(Base.Fix1(free_energy, t), ω)
-    g = dropdims(sum(f; dims = 1); dims = 1)
-    return g' * wₖ + e0
-end
-function free_energy(
-    t::AbstractVector{<:Temperature},
-    ω::AbstractArray{<:Frequency,4},
-    wₖ::AbstractVector,
-    e0::AbstractMatrix{<:Energy} = zeros(size(ω)[3:4]) * u"eV",
-)
-    f = map(t, eachslice(ω; dims = 3), eachrow(e0)) do t1, ω1, e1
-        free_energy(t1, ω1, wₖ, e1)
-    end
-    reshape(Iterators.flatten(f) |> collect, size(ω)[3:4])
+    sum(times(f, wₖ)) + e0
 end
 
+times(ω::DimensionalArray{T,2,<:Tuple{WaveVector,Branch}}, wₖ::Weights) where {T} = ω' * wₖ
+times(ω::DimensionalArray{T,2,<:Tuple{Branch,WaveVector}}, wₖ::Weights) where {T} = ω * wₖ
 function v_from_p(t0, ω, wk, e0, p, eos)
     f_t0v = free_energy(t0, ω, wk, e0)
     eos = lsqfit(eos(Collections.Energy()), volumes, f_t0v)
