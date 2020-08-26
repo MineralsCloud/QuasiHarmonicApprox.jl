fc1aa7363157344e18340b202ccdec7890bd7cd7
singularitti <singularitti@outlook.com>
2020-08-26 18:33:30 -0400

    Replace `DimensionalArray` with alias `DimArray`

--- src/SingleConfig.jl
+++ src/SingleConfig.jl
@@ -16,12 +16,11 @@ export WaveVector, Branch, Frequencies, Weights, times
 
 const FrequenciesAxes = Union{Tuple{WaveVector,Branch},Tuple{Branch,WaveVector}}
 
-const Frequencies =
-    DimensionalArray{<:Union{Frequency,Energy,Wavenumber},2,<:FrequenciesAxes}
-Frequencies(data, axes::FrequenciesAxes) = DimensionalArray(data, axes)
+const Frequencies = DimArray{<:Union{Frequency,Energy,Wavenumber},2,<:FrequenciesAxes}
+Frequencies(data, axes::FrequenciesAxes) = DimArray(data, axes)
 
-const Weights = DimensionalArray{<:Real,1,<:Tuple{WaveVector}}
-Weights(data, axes) = DimensionalArray(data, axes)
+const Weights = DimArray{<:Real,1,<:Tuple{WaveVector}}
+Weights(data, axes) = DimArray(data, axes)
 
 function free_energy(t::Temperature, ω::Frequencies, wₖ::Weights, e0::Energy = 0u"eV")
     @argcheck all(w >= zero(w) for w in wₖ)
@@ -40,8 +39,8 @@ function free_energy(
     return [free_energy(tt, ww, wₖ, e00) for tt in t, (ww, e00) in zip(ω, e0)]
 end
 
-times(ω::DimensionalArray{T,2,<:Tuple{WaveVector,Branch}}, wₖ::Weights) where {T} = ω' * wₖ
-times(ω::DimensionalArray{T,2,<:Tuple{Branch,WaveVector}}, wₖ::Weights) where {T} = ω * wₖ
+times(ω::DimArray{T,2,<:Tuple{WaveVector,Branch}}, wₖ::Weights) where {T} = ω' * wₖ
+times(ω::DimArray{T,2,<:Tuple{Branch,WaveVector}}, wₖ::Weights) where {T} = ω * wₖ
 
 function v_from_p(t0, ω, wk, e0, p, eosparam)
     f_t0v = free_energy(t0, ω, wk, e0)
