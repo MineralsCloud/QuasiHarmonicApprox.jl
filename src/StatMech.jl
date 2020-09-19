ac3a65cb2e283ea6c74b50d8ab32ba30e0cfddb5
singularitti <singularitti@outlook.com>
2020-09-19 04:31:49 -0400

    Remove all constraints of `Temperature`

--- src/StatMech.jl
+++ src/StatMech.jl
@@ -6,19 +6,18 @@ using Unitful: AbstractQuantity, NoUnits, ħ, k, c0, upreferred, dimension, @u_s
 export bose_einstein,
     partition_function, ho_free_energy, ho_internal_energy, ho_entropy, ho_vol_specific_heat
 
-const Temperature = AbstractQuantity{T,dimension(u"K")} where {T}
 const Frequency = AbstractQuantity{T,dimension(u"1/s")} where {T}
 const Energy = AbstractQuantity{T,dimension(u"J")} where {T}
 const Wavenumber = AbstractQuantity{T,dimension(u"1/m")} where {T}
 
-function bose_einstein(t::Temperature, ω::Frequency)
+function bose_einstein(t, ω::Frequency)
     @argcheck isreal(ω)
     return 1 / expm1(ħ * ω / (k * t))
 end
-bose_einstein(t::Temperature, e::Energy) = bose_einstein(t, _e2ω(e))
-bose_einstein(t::Temperature, ṽ::Wavenumber) = bose_einstein(t, _ṽ2ω(ṽ))
+bose_einstein(t, e::Energy) = bose_einstein(t, _e2ω(e))
+bose_einstein(t, ṽ::Wavenumber) = bose_einstein(t, _ṽ2ω(ṽ))
 
-function partition_function(t::Temperature, ω::Frequency)
+function partition_function(t, ω::Frequency)
     @argcheck isreal(ω)
     if iszero(ω)
         return 1
@@ -27,10 +26,10 @@ function partition_function(t::Temperature, ω::Frequency)
         return exp(x / 2) / expm1(x)
     end
 end
-partition_function(t::Temperature, e::Energy) = partition_function(t, _e2ω(e))
-partition_function(t::Temperature, ṽ::Wavenumber) = partition_function(t, _ṽ2ω(ṽ))
+partition_function(t, e::Energy) = partition_function(t, _e2ω(e))
+partition_function(t, ṽ::Wavenumber) = partition_function(t, _ṽ2ω(ṽ))
 
-function ho_free_energy(t::Temperature, ω::Frequency)
+function ho_free_energy(t, ω::Frequency)
     @argcheck isreal(ω)
     if iszero(ω)
         return upreferred(zero(ħ * ω))
@@ -39,10 +38,10 @@ function ho_free_energy(t::Temperature, ω::Frequency)
         return -ħω / 2 + kt * log(expm1(ħω / kt))
     end
 end
-ho_free_energy(t::Temperature, e::Energy) = ho_free_energy(t, _e2ω(e))
-ho_free_energy(t::Temperature, ṽ::Wavenumber) = ho_free_energy(t, _ṽ2ω(ṽ))
+ho_free_energy(t, e::Energy) = ho_free_energy(t, _e2ω(e))
+ho_free_energy(t, ṽ::Wavenumber) = ho_free_energy(t, _ṽ2ω(ṽ))
 
-function ho_internal_energy(t::Temperature, ω::Frequency)
+function ho_internal_energy(t, ω::Frequency)
     @argcheck isreal(ω)
     if iszero(ω)
         return k * t
@@ -51,17 +50,17 @@ function ho_internal_energy(t::Temperature, ω::Frequency)
         return ħω * coth(NoUnits(ħω / (k * t)))  # Can't use `ustrip`!
     end
 end
-ho_internal_energy(t::Temperature, e::Energy) = ho_internal_energy(t, _e2ω(e))
-ho_internal_energy(t::Temperature, ṽ::Wavenumber) = ho_internal_energy(t, _ṽ2ω(ṽ))
+ho_internal_energy(t, e::Energy) = ho_internal_energy(t, _e2ω(e))
+ho_internal_energy(t, ṽ::Wavenumber) = ho_internal_energy(t, _ṽ2ω(ṽ))
 
-function ho_entropy(t::Temperature, ω::Frequency)
+function ho_entropy(t, ω::Frequency)
     n = bose_einstein(t, ω)
     return k * ((1 + n) * log1p(n) - n * log(n))
 end
-ho_entropy(t::Temperature, e::Energy) = ho_entropy(t, _e2ω(e))
-ho_entropy(t::Temperature, ṽ::Wavenumber) = ho_entropy(t, _ṽ2ω(ṽ))
+ho_entropy(t, e::Energy) = ho_entropy(t, _e2ω(e))
+ho_entropy(t, ṽ::Wavenumber) = ho_entropy(t, _ṽ2ω(ṽ))
 
-function ho_vol_specific_heat(t::Temperature, ω::Frequency)
+function ho_vol_specific_heat(t, ω::Frequency)
     @argcheck isreal(ω)
     if iszero(ω)
         return k
@@ -70,8 +69,8 @@ function ho_vol_specific_heat(t::Temperature, ω::Frequency)
         return k * (x * csch(x))^2
     end
 end
-ho_vol_specific_heat(t::Temperature, e::Energy) = ho_vol_specific_heat(t, _e2ω(e))
-ho_vol_specific_heat(t::Temperature, ṽ::Wavenumber) = ho_vol_specific_heat(t, _ṽ2ω(ṽ))
+ho_vol_specific_heat(t, e::Energy) = ho_vol_specific_heat(t, _e2ω(e))
+ho_vol_specific_heat(t, ṽ::Wavenumber) = ho_vol_specific_heat(t, _ṽ2ω(ṽ))
 
 _e2ω(e::Energy) = e / ħ
 
