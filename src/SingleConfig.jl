e4f88246586c70dcc83c125bce0e177c60709cb5
singularitti <singularitti@outlook.com>
2020-09-24 04:02:02 -0400

    Add function `testconverge` in src/SingleConfig.jl

--- src/SingleConfig.jl
+++ src/SingleConfig.jl
@@ -20,6 +20,14 @@ const Vol = Dim{:Vol}
 const Press = Dim{:Press}
 const NormalMode = Union{Tuple{Wavevector,Branch},Tuple{Branch,Wavevector}}
 
+function testconverge(t, ωs, wₖs, N = 3)
+    perm = sortperm(wₖs; by = length)
+    fe = map(ωs[perm[(end-N+1):end]], wₖs[perm[(end-N+1):end]]) do ω, wₖ
+        ho_free_energy(t, ω, wₖ)
+    end
+    return all(y / x < 1 for (x, y) in zip(fe, fe[2:end]))
+end
+
 function ho_free_energy(t, ω::AbstractDimMatrix{T,<:NormalMode}, wₖ) where {T}
     wₖ = wₖ ./ sum(wₖ)  # Normalize weights
     fₕₒ = ho_free_energy.(t, ω)  # free energy on each harmonic oscillator
