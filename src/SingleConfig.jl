2e7f3b77bb42aac36b3146b3a3b10a6cee728580
singularitti <singularitti@outlook.com>
2020-09-24 04:18:44 -0400

    Move things from src/SingleConfig.jl to src/Thermo.jl

--- src/SingleConfig.jl
+++ src/SingleConfig.jl
@@ -1,23 +1,14 @@
 module SingleConfig
 
-using DimensionalData:
-    AbstractDimMatrix, AbstractDimVector, DimArray, Dim, dims, swapdims, rebuild
-using EquationsOfStateOfSolids.Collections:
-    Parameters, BirchMurnaghan3rd, EnergyEOS, PressureEOS
-using EquationsOfStateOfSolids.Fitting: eosfit
-using EquationsOfStateOfSolids.Volume: mustfindvolume
-using Unitful: Energy
+using DimensionalData: AbstractDimMatrix, Dim
 
 import DimensionalData
 import ..StatMech: ho_free_energy
 
-export Wavevector, Branch, Temp, Vol, v2p
+export Wavevector, Branch
 
 const Wavevector = Dim{:Wavevector}
 const Branch = Dim{:Branch}
-const Temp = Dim{:Temp}
-const Vol = Dim{:Vol}
-const Press = Dim{:Press}
 const NormalMode = Union{Tuple{Wavevector,Branch},Tuple{Branch,Wavevector}}
 
 function testconverge(t, ωs, wₖs, N = 3)
@@ -44,38 +35,7 @@ end
 sample_bz(ω::AbstractDimMatrix{T,<:Tuple{Wavevector,Branch}}, wₖ) where {T} =
     sample_bz(transpose(ω), wₖ)  # Just want to align axis, `transpose` is enough.
 
-function v2p(fₜ₀ᵥ::AbstractDimVector{<:Energy,<:Tuple{Vol}}, param0::Parameters)
-    volumes = dims(fₜ₀ᵥ, Vol)
-    param = eosfit(EnergyEOS(param0), volumes, fₜ₀ᵥ)
-    _v2p() = swapdims(fₜ₀ᵥ, (Press(map(PressureEOS(param), volumes)),))  # `swapdims` will keep `refdims`
-    function _v2p(pressures)
-        fₜ₀ₚ = map(pressures) do p0
-            v = mustfindvolume(PressureEOS(param), p0)
-            EnergyEOS(param)(v)
-        end
-        return rebuild(fₜ₀ᵥ, fₜ₀ₚ, (Press(pressures),))
-    end
-    return _v2p
-end
-function v2p(
-    fₜᵥ::AbstractDimMatrix{<:Energy,<:Union{Tuple{Temp,Vol},Tuple{Vol,Temp}}},
-    param0::Parameters,
-)
-    function _v2p()
-        arr = map(fₜ₀ᵥ -> v2p(fₜ₀ᵥ, param0)(), eachslice(fₜᵥ; dims = Temp))
-        return DimArray(arr, dims(fₜᵥ, (Temp,)))
-    end
-    function _v2p(pressures)
-        arr = map(fₜ₀ᵥ -> v2p(fₜ₀ᵥ, param0)(pressures), eachslice(fₜᵥ; dims = Temp))
-        return DimArray(arr, dims(fₜᵥ, (Temp,)))
-    end
-    return _v2p
-end
-
 DimensionalData.name(::Type{<:Wavevector}) = "Wavevector"
 DimensionalData.name(::Type{<:Branch}) = "Branch"
-DimensionalData.name(::Type{<:Vol}) = "Volume"
-DimensionalData.name(::Type{<:Temp}) = "Temperature"
-DimensionalData.name(::Type{<:Press}) = "Pressure"
 
 end
