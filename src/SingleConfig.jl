f7323921554cf56cd7fc8b223157caf9bab347b7
singularitti <singularitti@outlook.com>
2020-09-24 04:31:19 -0400

    Fix `sample_bz`

--- src/SingleConfig.jl
+++ src/SingleConfig.jl
@@ -23,17 +23,18 @@ function ho_free_energy(t, ω::AbstractDimMatrix{T,<:NormalMode}, wₖ) where {T
     wₖ = wₖ ./ sum(wₖ)  # Normalize weights
     fₕₒ = ho_free_energy.(t, ω)  # free energy on each harmonic oscillator
     return sum(sample_bz(fₕₒ, wₖ))  # Scalar
+    return sample_bz(fₕₒ, wₖ)  # Scalar
 end
 
 # Relax the constraint on wₖ, it can even be a 2×1 matrix!
-function sample_bz(ω::AbstractDimMatrix{T,<:Tuple{Branch,Wavevector}}, wₖ) where {T}
+function sample_bz(xₙₖ::AbstractDimMatrix{T,<:Tuple{Branch,Wavevector}}, wₖ) where {T}
     if any(wₖ .<= zero(eltype(wₖ)))  # Must hold, or else wₖ is already wrong
         throw(DomainError("All the values of the weights should be greater than 0!"))
     end
-    return ω * collect(wₖ)  # Allow wₖ to be a tuple
+    return sum(xₙₖ * collect(wₖ))  # `collect` allows wₖ to be a tuple
 end
-sample_bz(ω::AbstractDimMatrix{T,<:Tuple{Wavevector,Branch}}, wₖ) where {T} =
-    sample_bz(transpose(ω), wₖ)  # Just want to align axis, `transpose` is enough.
+sample_bz(xₖₙ::AbstractDimMatrix{T,<:Tuple{Wavevector,Branch}}, wₖ) where {T} =
+    sample_bz(transpose(xₖₙ), wₖ)  # Just want to align axis, `transpose` is enough.
 
 DimensionalData.name(::Type{<:Wavevector}) = "Wavevector"
 DimensionalData.name(::Type{<:Branch}) = "Branch"
