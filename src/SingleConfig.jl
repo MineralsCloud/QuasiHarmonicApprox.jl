8e0a274fe653acb6239342681fde734b5fd465cc
singularitti <singularitti@outlook.com>
2020-10-04 18:38:47 -0400

    Deprecate old type constraints, rewrite `TempIndependentNormalModes` & `TempDependentNormalModes`
    to `TempIndepNormalModes` (1D) & `TempDepNormalModes` (2D)

--- src/SingleConfig.jl
+++ src/SingleConfig.jl
@@ -1,13 +1,29 @@
 module SingleConfig
 
 using DimensionalData:
-    AbstractDimArray, AbstractDimMatrix, AbstractDimVector, DimArray, Dim, dims, swapdims
+    AbstractDimArray,
+    AbstractDimMatrix,
+    AbstractDimVector,
+    DimArray,
+    Dim,
+    dims,
+    swapdims,
+    hasdim
+using OptionalArgChecks: @argcheck
 using Unitful: Temperature, Energy
 
 import DimensionalData
 import ..StatMech: ho_free_energy, ho_internal_energy, ho_entropy, ho_vol_sp_ht
 
-export Wavevector, Branch, Temp, Vol, Press, FreeEnergy, sample_bz
+export Wavevector,
+    Branch,
+    Temp,
+    Vol,
+    Press,
+    FreeEnergy,
+    TempIndepNormalModes,
+    TempDepNormalModes,
+    sample_bz
 
 const Wavevector = Dim{:Wavevector}  # TODO: Should I add more constraints?
 const Branch = Dim{:Branch}
@@ -18,54 +34,14 @@ const NormalModes = AbstractDimMatrix{
     T,
     <:Union{Tuple{Wavevector,Branch},Tuple{Branch,Wavevector}},
 } where {T}
-const TempIndependentNormalModes = Union{
-    AbstractDimArray{
-        T,
-        3,
-        <:Union{
-            Tuple{Wavevector,Branch,Vol},
-            Tuple{Branch,Wavevector,Vol},
-            Tuple{Vol,Branch,Wavevector},
-            Tuple{Vol,Wavevector,Branch},
-            Tuple{Wavevector,Vol,Branch},
-            Tuple{Branch,Vol,Wavevector},
-        },
-    },
-    AbstractDimVector{<:NormalModes,<:Tuple{Vol}},
-} where {T}
-const TempDependentNormalModes = Union{
-    AbstractDimArray{
-        T,
-        4,
-        <:Union{
-            Tuple{Wavevector,Branch,Vol,Temp},
-            Tuple{Wavevector,Branch,Temp,Vol},
-            Tuple{Wavevector,Vol,Branch,Temp},
-            Tuple{Wavevector,Vol,Temp,Branch},
-            Tuple{Wavevector,Temp,Branch,Vol},
-            Tuple{Wavevector,Temp,Vol,Branch},
-            Tuple{Branch,Wavevector,Vol,Temp},
-            Tuple{Branch,Wavevector,Temp,Vol},
-            Tuple{Branch,Vol,Wavevector,Temp},
-            Tuple{Branch,Vol,Temp,Wavevector},
-            Tuple{Branch,Temp,Wavevector,Vol},
-            Tuple{Branch,Temp,Vol,Wavevector},
-            Tuple{Vol,Wavevector,Branch,Temp},
-            Tuple{Vol,Wavevector,Temp,Branch},
-            Tuple{Vol,Branch,Wavevector,Temp},
-            Tuple{Vol,Branch,Temp,Wavevector},
-            Tuple{Vol,Temp,Wavevector,Branch},
-            Tuple{Vol,Temp,Branch,Wavevector},
-            Tuple{Temp,Wavevector,Branch,Vol},
-            Tuple{Temp,Wavevector,Vol,Branch},
-            Tuple{Temp,Branch,Wavevector,Vol},
-            Tuple{Temp,Branch,Vol,Wavevector},
-            Tuple{Temp,Vol,Wavevector,Branch},
-            Tuple{Temp,Vol,Branch,Wavevector},
-        },
-    },
-    AbstractDimMatrix{<:NormalModes,<:Union{Tuple{Temp,Vol},Tuple{Vol,Temp}}},
-} where {T}
+const TempVolOrVolTemp = Union{Tuple{Temp,Vol},Tuple{Vol,Temp}}
+const TempIndepNormalModes = AbstractDimVector{<:NormalModes,<:Tuple{Vol}}
+const TempDepNormalModes = AbstractDimMatrix{<:NormalModes,<:TempVolOrVolTemp}
+const TempVolOrVolTempField = AbstractDimMatrix{T,<:TempVolOrVolTemp} where {T}
+const FreeEnergy = DimArray{<:Energy,2,<:TempVolOrVolTemp}
+const InternalEnergy = DimArray{<:Energy,2,<:TempVolOrVolTemp}
+const Entropy = DimArray{T,2,<:TempVolOrVolTemp} where {T}
+const VolSpHt = DimArray{T,2,<:TempVolOrVolTemp} where {T}
 
 function testconverge(t, ωs, wₖs, N = 3)
     perm = sortperm(wₖs; by = length)
@@ -75,28 +51,36 @@ function testconverge(t, ωs, wₖs, N = 3)
     return all(y / x < 1 for (x, y) in zip(fe, fe[2:end]))
 end
 
-const FreeEnergy = DimArray{<:Energy,2,<:Union{Tuple{Temp,Vol},Tuple{Vol,Temp}}}
-const InternalEnergy = DimArray{<:Energy,2,<:Union{Tuple{Temp,Vol},Tuple{Vol,Temp}}}
-const Entropy = DimArray{T,2,<:Union{Tuple{Temp,Vol},Tuple{Vol,Temp}}} where {T}
-const VolSpHt = DimArray{T,2,<:Union{Tuple{Temp,Vol},Tuple{Vol,Temp}}} where {T}
+function TempIndepNormalModes(ω::AbstractDimArray{T,3})::TempIndepNormalModes where {T}
+    @argcheck all(hasdim(ω, (Branch, Wavevector, Vol)))
+    return DimArray([ωᵥ for ωᵥ in eachslice(ω; dims = Vol)], dims(ω, Vol))
+end
+
+function TempDepNormalModes(ω::AbstractDimArray{T,4})::TempDepNormalModes where {T}
+    @argcheck all(hasdim(ω, (Branch, Wavevector, Vol, Temp)))
+    M, N = map(Base.Fix1(size, ω), (Temp, Vol))
+    return DimArray([ω[Temp(i), Vol(j)] for i in 1:M, j in 1:N], dims(ω, (Temp, Vol)))
+end
 
 for (T, f) in zip(
     (:FreeEnergy, :InternalEnergy, :Entropy, :VolSpHt),
     (:ho_free_energy, :ho_internal_energy, :ho_entropy, :ho_vol_sp_ht),
 )
     expr = quote
-        function $T(ω::TempIndependentNormalModes, wₖ, axes)
-            t, v = dims(axes, (Temp, Vol))
+        function $T(ω::TempIndepNormalModes, wₖ, axes)::TempVolOrVolTempField
+            t, v = dims(axes, Temp), Base.Fix2(dims, Vol)(hasdim(axes, Vol) ? axes : ω)
             M, N = map(length, (t, v))
-            arr = [sample_bz(x -> $f(t[i], x), ω[Vol(j)], wₖ) for i in 1:M, j in 1:N]  # Slower than `eachslice(ω; dims = Vol)`
+            arr = [sample_bz(x -> $f(t[i], x), ω[j], wₖ) for i in 1:M, j in 1:N]  # Slower than `eachslice(ω; dims = Vol)`
             return swapdims(DimArray(reshape(arr, (M, N)), (t, v)), axes)
         end
-        function $T(ω::TempDependentNormalModes, wₖ, axes)  # FIXME: fix `ω[Vol(i)]` method error
+        function $T(
+            ω::TempDepNormalModes,
+            wₖ,
+            axes = dims(ω, (Temp, Vol)),
+        )::TempVolOrVolTempField
             t, v = dims(axes, (Temp, Vol))
             M, N = map(length, (t, v))
-            arr = [
-                sample_bz(x -> $f(t[i], x), ω[Temp(i), Vol(j)], wₖ) for i in 1:M, j in 1:N
-            ]  # `eachslice` is not easy to use here
+            arr = [sample_bz(x -> $f(t[i], x), ω[i, j], wₖ) for i in 1:M, j in 1:N]  # `eachslice` is not easy to use here
             return swapdims(DimArray(arr, (t, v)), axes)
         end
     end
