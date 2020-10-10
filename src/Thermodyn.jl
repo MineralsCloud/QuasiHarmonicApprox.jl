455b087bf7a56f4fd1debcc0bcd710b057bef3c9
singularitti <singularitti@outlook.com>
2020-10-10 19:12:25 -0400

    Simplify `v2p` by removing `_v2p`

--- src/Thermodyn.jl
+++ src/Thermodyn.jl
@@ -13,27 +13,26 @@ export v2p
 
 const TempVolOrVolTempField = AbstractDimMatrix{T,<:TempVolOrVolTemp} where {T}
 
-function v2p(eos::EnergyEOS, fₜ₀ᵥ::AbstractDimVector{<:Energy,<:Tuple{Vol}})
+function v2p(param::Parameters, fₜ₀ᵥ::AbstractDimVector{<:Energy,<:Tuple{Vol}})
     volumes = dims(fₜ₀ᵥ, Vol)
-    param = eosfit(eos, volumes, fₜ₀ᵥ)
-    function _v2p(pressures)
+    param = eosfit(EnergyEOS(param), volumes, fₜ₀ᵥ)
+    return function (pressures)
         fₜ₀ₚ = map(pressures) do pressure
             v = mustfindvolume(PressureEOS(param), pressure)
             EnergyEOS(param)(v)
         end
         return rebuild(fₜ₀ᵥ, fₜ₀ₚ, (Press(pressures),))
     end
-    return _v2p
 end
-function v2p(eos::EnergyEOS, fₜᵥ::TempVolOrVolTempField{<:Energy})
-    function _v2p(pressures)
-        arr = map(fₜ₀ᵥ -> v2p(eos, fₜ₀ᵥ)(pressures), eachslice(fₜᵥ; dims = Temp))
+function v2p(param::Parameters, fₜᵥ::TempVolOrVolTempField)
+    return function (pressures)
+        arr = map(fₜ₀ᵥ -> v2p(param, fₜ₀ᵥ)(pressures), eachslice(fₜᵥ; dims = Temp))
         mat = hcat(arr...)'
         ax = dims(fₜᵥ)
         x = swapdims(DimArray(mat, (dims(fₜᵥ, Temp), Press(pressures))), map(typeof, ax))
         return set(x, Vol = Press(pressures))
     end
-    return _v2p
+end
 end
 
 end
