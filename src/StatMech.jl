e468c705baf16c62e59253a8796f5ec3a16dec5c
singularitti <singularitti@outlook.com>
2021-01-01 01:13:42 -0500

    Only allow `Real` frequencies

--- src/StatMech.jl
+++ src/StatMech.jl
@@ -6,14 +6,14 @@ using Unitful: Frequency, Energy, Wavenumber, NoUnits, J, ħ, k, c0, upreferred
 export bose_einstein,
     partition_function, ho_free_energy, ho_internal_energy, ho_entropy, ho_vol_sp_ht
 
-function bose_einstein(t, ω::Frequency)
-    @argcheck checkfreq(ω)
+function bose_einstein(t, ω::Frequency{<:Real})
+    @argcheck isnonnegative(ω)
     return 1 / expm1(ħ * ω / (k * t))
 end
 bose_einstein(t, x) = bose_einstein(t, tofreq(x))
 
-function partition_function(t, ω::Frequency)
-    @argcheck checkfreq(ω)
+function partition_function(t, ω::Frequency{<:Real})
+    @argcheck isnonnegative(ω)
     if iszero(ω)
         return 1
     else
@@ -23,8 +23,8 @@ function partition_function(t, ω::Frequency)
 end
 partition_function(t, x) = partition_function(t, tofreq(x))
 
-function ho_free_energy(t, ω::Frequency)
-    @argcheck checkfreq(ω)
+function ho_free_energy(t, ω::Frequency{<:Real})
+    @argcheck isnonnegative(ω)
     if iszero(ω)
         return 0 * upreferred(J)  # `upreferred` is required to make it fast for arrays
     else
@@ -34,8 +34,8 @@ function ho_free_energy(t, ω::Frequency)
 end
 ho_free_energy(t, x) = ho_free_energy(t, tofreq(x))
 
-function ho_internal_energy(t, ω::Frequency)
-    @argcheck checkfreq(ω)
+function ho_internal_energy(t, ω::Frequency{<:Real})
+    @argcheck isnonnegative(ω)
     if iszero(ω)
         return k * t
     else
@@ -45,14 +45,14 @@ function ho_internal_energy(t, ω::Frequency)
 end
 ho_internal_energy(t, x) = ho_internal_energy(t, tofreq(x))
 
-function ho_entropy(t, ω::Frequency)
+function ho_entropy(t, ω::Frequency{<:Real})
     n = bose_einstein(t, ω)
     return k * ((1 + n) * log1p(n) - n * log(n))
 end
 ho_entropy(t, x) = ho_entropy(t, tofreq(x))
 
-function ho_vol_sp_ht(t, ω::Frequency)
-    @argcheck checkfreq(ω)
+function ho_vol_sp_ht(t, ω::Frequency{<:Real})
+    @argcheck isnonnegative(ω)
     if iszero(ω)
         return k
     else
@@ -65,6 +65,6 @@ ho_vol_sp_ht(t, x) = ho_vol_sp_ht(t, tofreq(x))
 tofreq(e::Energy) = e / ħ  # Do not export!
 tofreq(ṽ::Wavenumber) = ṽ * c0  # Do not export!
 
-checkfreq(ω) = isreal(ω) && ω >= zero(ω)
+isnonnegative(ω) = ω >= zero(ω)
 
 end
