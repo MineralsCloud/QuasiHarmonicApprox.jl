632e0ffe76e0c3593b0a61fec7bb6343de0e4bcc
singularitti <singularitti@outlook.com>
2022-09-01 16:34:35 -0400

    Add more constructors for `HarmonicOscillator` to replace `to_ω`

--- src/StatMech.jl
+++ src/StatMech.jl
@@ -7,13 +7,16 @@ export HarmonicOscillator
 export bose_einstein_dist,
     partition_function, free_energy, internal_energy, entropy, volumetric_heat_capacity
 
-struct HarmonicOscillator{T<:Frequency}
+struct HarmonicOscillator{T<:Number}
     ω::T
-    function HarmonicOscillator(ω)
+    function HarmonicOscillator{T}(ω) where {T<:Frequency}
         @assert ω >= zero(ω)
         return new(ω)
     end
 end
+HarmonicOscillator(ω::T) where {T<:Frequency} = HarmonicOscillator{T}(ω)
+HarmonicOscillator(e::Energy) = HarmonicOscillator(e / ħ)
+HarmonicOscillator(k::Wavenumber) = HarmonicOscillator(k * c0)
 @functor HarmonicOscillator
 
 bose_einstein_dist(ho::HarmonicOscillator, t) = 1 / expm1(ħ * ho.ω / (k * t))
