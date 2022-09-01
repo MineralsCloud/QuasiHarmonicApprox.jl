06f314362b1c1a319c268754c3eec2b9bb02d784
singularitti <singularitti@outlook.com>
2022-09-01 15:27:50 -0400

    Update docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,28 @@
 using QuasiHarmonicApprox
 using Documenter
 
+DocMeta.setdocmeta!(QuasiHarmonicApprox, :DocTestSetup, :(using QuasiHarmonicApprox); recursive=true)
+
 makedocs(;
     modules=[QuasiHarmonicApprox],
-    authors="Qi Zhang <singularitti@outlook.com>",
-    repo="https://github.com/MineralsCloud/QuasiHarmonicApprox.jl/blob/{commit}{path}#L{line}",
+    authors="singularitti <singularitti@outlook.com>",
+    repo="https://github.com/MineralsCloud/QuasiHarmonicApprox.jl/blob/{commit}{path}#{line}",
     sitename="QuasiHarmonicApprox.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://MineralsCloud.github.io/QuasiHarmonicApprox.jl",
+        edit_link="main",
         assets=String[],
     ),
     pages=[
         "Home" => "index.md",
+        "Manual" => ["Installation guide" => "installation.md"],
+        "API Reference" => [],
+        "Developer Docs" => [
+            "Contributing" => "developers/contributing.md",
+            "Style Guide" => "developers/style.md",
+        ],
+        "Troubleshooting" => "troubleshooting.md",
     ],
 )
 
